421762328eb95af6e0bf0db1fcda0b74a36d2473
nikhilprashar561 <nikhilprashar561@gmail.com>
2025-11-02 12:18:33 +0530

    js

--- practice.js
+++ practice.js
@@ -2,25 +2,24 @@ const data = [
     {
         name:"Nikhil",
         className:"BSC",
-        roll_no:"",
-        profession:""
+        roll_no:253805,
+        profession:"Developer"
     },
     {
-        name:"",
-        className:"",
-        roll_no:"",
-        profession:""
+        name:"Raja",
+        className:"BCA",
+        roll_no:253806,
+        profession:"Police"
     },
     {
-        name:"",
-        className:"",
-        roll_no:"",
-        profession:""
-    },
-    {
-        name:"",
-        className:"",
-        roll_no:"",
-        profession:""
+        name:"Yuvraj",
+        className:"BA",
+        roll_no:253837,
+        profession:"MBA"
     }
 ]
+
+data.forEach((items, index)=> {
+    let obj = Object.values(items)
+    console.log(`yeh hai items ${obj} aur yeh hai index ${index}`)
+}) 
